ef02afd8de5af5e3f925fc57dcf493b2cba1c62d
Peter Li <42709877+eini122@users.noreply.github.com>
2019-04-18 22:43:44 -0500

    Junit test

---
+++ Junit/AllTests.java
@@ -0,0 +1,11 @@
+package tipsCalculator;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ Test_getTipsPercent.class, test_getTipsValue.class, test_getTotal.class })
+public class AllTests {
+
+}
